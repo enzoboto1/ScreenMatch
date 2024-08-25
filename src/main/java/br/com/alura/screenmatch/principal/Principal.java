387f2eab3c85047d360a0fefa36ab21ac18292f5
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-25 15:06:17 -0300

    Criando Classe Principal

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporadas;
+import br.com.alura.screenmatch.model.Episodio;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Principal {
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    private ConverteDados conversor = new ConverteDados();
+    private Scanner scanner = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+    public void exibeMenu(){
+
+    }
+
+}
