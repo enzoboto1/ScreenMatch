e1f5c75393b68f214d7d91c0bc7adb262c1030e7
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-27 23:45:43 -0300

    Ajustes em Serie

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,9 +1,12 @@
 package br.com.alura.screenmatch.model;
+
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
 import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
+
 @Entity
 @Table(name = "series")
 public class Serie {
@@ -19,11 +22,11 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
+
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
@@ -33,60 +36,80 @@ public class Serie {
         this.poster = dadosSerie.poster();
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public List<Episodio> getEpisodios() {
         return episodios;
     }
+
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
+
     public String getTitulo() {
         return titulo;
     }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
     public Integer getTotalTemporadas() {
         return totalTemporadas;
     }
+
     public void setTotalTemporadas(Integer totalTemporadas) {
         this.totalTemporadas = totalTemporadas;
     }
+
     public Double getAvaliacao() {
         return avaliacao;
     }
+
     public void setAvaliacao(Double avaliacao) {
         this.avaliacao = avaliacao;
     }
+
     public Categoria getGenero() {
         return genero;
     }
+
     public void setGenero(Categoria genero) {
         this.genero = genero;
     }
+
     public String getAtores() {
         return atores;
     }
+
     public void setAtores(String atores) {
         this.atores = atores;
     }
+
     public String getPoster() {
         return poster;
     }
+
     public void setPoster(String poster) {
         this.poster = poster;
     }
+
     public String getSinopse() {
         return sinopse;
     }
+
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
+
     @Override
     public String toString() {
         return
@@ -96,6 +119,7 @@ public class Serie {
                         ", avaliacao=" + avaliacao +
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
-                        ", sinopse='" + sinopse + '\'';
+                        ", sinopse='" + sinopse + '\'' +
+                        ", episodios='" + episodios + '\'';
     }
 }
