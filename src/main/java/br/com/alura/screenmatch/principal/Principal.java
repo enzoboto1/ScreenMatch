7f91e742dbaab039a13aca9364764f68842acd69
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-27 23:28:00 -0300

    Adicionando SerieRepository em Principal

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,8 +1,8 @@
 package br.com.alura.screenmatch.principal;
-
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -11,15 +11,19 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
 public class Principal {
-
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
     private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+    private SerieRepository repositorio;
+
+    public Principal(SerieRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void exibeMenu() {
         var opcao = -1;
@@ -31,11 +35,9 @@ public class Principal {
                                     
                     0 - Sair                                 
                     """;
-
             System.out.println(menu);
             opcao = leitura.nextInt();
             leitura.nextLine();
-
             switch (opcao) {
                 case 1:
                     buscarSerieWeb();
@@ -58,6 +60,9 @@ public class Principal {
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
         dadosSeries.add(dados);
+        Serie serie = new Serie(dados);
+        //dadosSeries.add(dados);
+        repositorio.save(serie);
         System.out.println(dados);
     }
 
@@ -68,11 +73,9 @@ public class Principal {
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         return dados;
     }
-
     private void buscarEpisodioPorSerie(){
         DadosSerie dadosSerie = getDadosSerie();
         List<DadosTemporada> temporadas = new ArrayList<>();
-
         for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
             var json = consumo.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
@@ -80,7 +83,6 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
     }
-
     private void listarSeriesBuscadas(){
         List<Serie> series = new ArrayList<>();
         series = dadosSeries.stream()
