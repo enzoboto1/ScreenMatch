e8cca35c60f172c591bf4363199fe2e1f9734805
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-27 23:27:12 -0300

    Adicionando configurações de tabelas em Serie

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,18 +1,24 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
     private String sinopse;
-
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
@@ -23,62 +29,60 @@ public class Serie {
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
+    public Serie() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitulo() {
         return titulo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
     public Integer getTotalTemporadas() {
         return totalTemporadas;
     }
-
     public void setTotalTemporadas(Integer totalTemporadas) {
         this.totalTemporadas = totalTemporadas;
     }
-
     public Double getAvaliacao() {
         return avaliacao;
     }
-
     public void setAvaliacao(Double avaliacao) {
         this.avaliacao = avaliacao;
     }
-
     public Categoria getGenero() {
         return genero;
     }
-
     public void setGenero(Categoria genero) {
         this.genero = genero;
     }
-
     public String getAtores() {
         return atores;
     }
-
     public void setAtores(String atores) {
         this.atores = atores;
     }
-
     public String getPoster() {
         return poster;
     }
-
     public void setPoster(String poster) {
         this.poster = poster;
     }
-
     public String getSinopse() {
         return sinopse;
     }
-
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
-
     @Override
     public String toString() {
         return
@@ -86,7 +90,6 @@ public class Serie {
                         ", titulo='" + titulo + '\'' +
                         ", totalTemporadas=" + totalTemporadas +
                         ", avaliacao=" + avaliacao +
-
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
                         ", sinopse='" + sinopse + '\'';
