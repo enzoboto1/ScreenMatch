33936ce7d49ca579fae237aee64bffb1de04b23a
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-29 23:09:08 -0300

    Adicionando anotação @RestController em @RestController

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -3,7 +3,7 @@ package br.com.alura.screenmatch.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class SerieController {
 
 }
