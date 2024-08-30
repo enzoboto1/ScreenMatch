30f2e4b0fe7f9f57b0fd3d6e3b49bbaa44f72ca1
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-30 00:02:54 -0300

    Ajuste no RequestMapping - SerieController

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,26 +1,26 @@
 package br.com.alura.screenmatch.controller;
-
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
-    @Autowired
     private SerieService servico;
 
-    @GetMapping("/series")
+    public SerieController(SerieService servico){
+        this.servico = servico;
+    }
+
+    @GetMapping
     public List<SerieDTO> obterSeries() {
         return servico.obterTodasAsSeries();
     }
 
 }
-
