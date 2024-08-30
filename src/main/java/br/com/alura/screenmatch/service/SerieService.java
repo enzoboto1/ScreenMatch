3028924d7a94b2805884a79f680b878f39062d0e
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-29 23:32:48 -0300

    Adicionando metodo obterTodasAsSeries na classe SerieService

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.service;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SerieService {
 
@@ -9,5 +13,12 @@ public class SerieService {
 
     public SerieService(SerieRepository repositorio){
         this.repositorio = repositorio;
+    }
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
 }
