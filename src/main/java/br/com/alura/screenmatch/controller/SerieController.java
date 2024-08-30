ef42259a6342361242eec4e25668e2fb5c3d3186
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-29 23:10:24 -0300

    Adicionando metodo obterSeries em SerieController

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -6,5 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SerieController {
 
+    @GetMapping("/series")
+    public String obterSeries() {
+        return "Aqui vão ser listadas as séries";
+    }
 }
 
