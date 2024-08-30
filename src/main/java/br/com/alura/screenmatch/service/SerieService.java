d7868f0975b73c4d3ebfb7b2974070890ea679ce
enzoboto1 <enzoboto1.0@gmail.com>
2024-08-29 23:31:51 -0300

    Adicionando construtor classe SerieService

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,4 +1,13 @@
 package br.com.alura.screenmatch.service;
 
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SerieService {
+
+    private SerieRepository repositorio;
+
+    public SerieService(SerieRepository repositorio){
+        this.repositorio = repositorio;
+    }
 }
