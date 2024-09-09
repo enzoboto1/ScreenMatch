6cd21fa6cf4c42242a66d3cf6dea701aeb886a2f
enzoboto1 <enzoboto1.0@gmail.com>
2024-09-09 08:58:29 -0300

    Espacamento

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -11,5 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreenmatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
+
     }
 }
