48b3f65edf5796f9faa8a23f416d00a88a13244d
enzoboto1 <enzoboto1.0@gmail.com>
2024-09-09 08:54:03 -0300

    Espacamento

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -16,7 +16,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
         Principal principal = new Principal();
         principal.exibeMenu();
-
 	}
 
 }
